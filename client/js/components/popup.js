3cb2b131be5c0acc1ad3d736c69fddc835cf8c1b
owenzh <zhangxudong_sun@sina.com>
2018-09-05 16:37:18 +0800

    popup init

--- client/js/components/popup.js
+++ client/js/components/popup.js
@@ -1,32 +1,53 @@
 import React,{Component} from 'react';
+// import ReactDom from 'react-dom'
 
 class Popup extends Component{
     constructor(props) {
         super(props);
-        // this.state = {show: false};
-        this.popupid = 'model-'+new Date().valueOf();
+        this.popupid = 'popup-'+new Date().valueOf();
+        this.coverdiv = null;
+        this.popupdiv = null;
+        this.closePopup = this.close.bind(this);
         console.log(this.popupid);
     }
     componentWillMount() {
-        console.log('Component WILL MOUNT!')
+        // console.log('Component WILL MOUNT!')
     }
     componentDidMount() {
-         console.log('Component DID MOUNT!')
+        //  console.log('Component DID MOUNT!')
     }
     componentWillReceiveProps(newProps) {
-          console.log('Component WILL RECEIVE PROPS!' + newProps)
+          console.log('Component WILL RECEIVE PROPS!');
+          if(newProps.show){
+            this.show();
+          }
     }
     shouldComponentUpdate(newProps, newState) {
           return true;
     }
     componentWillUpdate(nextProps, nextState) {
-          console.log('Component WILL UPDATE!');
+        //   console.log('Component WILL UPDATE!');
     }
     componentDidUpdate(prevProps, prevState) {
-          console.log('Component DID UPDATE!')
+        //   console.log('Component DID UPDATE!')
     }
     componentWillUnmount() {
-           console.log('Component WILL UNMOUNT!')
+        //    console.log('Component WILL UNMOUNT!')
+    }
+    close() {
+        this.coverdiv.setAttribute('style', 'display:none');
+        this.popupdiv.setAttribute('style', 'display:none');
+    }
+    show() {
+        let popupContainer = document.getElementById(this.popupid);
+        this.coverdiv = popupContainer.getElementsByClassName('xu-cover')[0];
+        this.popupdiv = popupContainer.getElementsByClassName('xu-popup')[0];
+        this.coverdiv.setAttribute('style', 'display:block');
+        this.popupdiv.setAttribute('style', 'display:block');
+        let clasNm = this.popupdiv.className;
+        if (clasNm.indexOf('xu-popup-show') == -1) {
+            this.popupdiv.className = clasNm + ' xu-popup-show';
+        }
     }
     render(){
         return (
@@ -34,8 +55,15 @@ class Popup extends Component{
                 <div id={this.popupid}>
                     <div className="xu-cover">
                     </div>
-                    <div className="xu-popup">
-                        {this.props.content}
+                    <div className="xu-popup xu-round-edge">
+                        <div className="xu-popup-header xu-round-edge-lr">
+                            <span onClick={this.closePopup}>[x]</span>
+                        </div>
+                        <div className="xu-popup-content">
+                            {this.props.content}
+                        </div>
+                        <div className="xu-popup-footer">
+                        </div>
                     </div>
                 </div>
             </React.Fragment>
