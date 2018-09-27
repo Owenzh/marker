efe89cbce86cfa4fc993cf6e7373873e880e3d39
owenzh <zhangxudong_sun@sina.com>
2018-09-27 16:29:22 +0800

    [update] create popup.css

--- client/js/layout/header.js
+++ client/js/layout/header.js
@@ -10,15 +10,15 @@ class Header extends Component{
     if(id=="1"){
       createItemModel = (
         <div>
-          <h1>111</h1>
-          <button onClick={this.createItem}></button>
+          <h1>创建类别</h1>
+          <button onClick={this.createCategory}>创建</button>
         </div>
       );
     }else{
       createItemModel = (
         <div>
-          <h1>222</h1>
-          <button onClick={this.createItem}></button>
+          <h1>创建条目</h1>
+          <button onClick={this.createItem}>创建</button>
         </div>
       );
     }
@@ -27,6 +27,9 @@ class Header extends Component{
   createItem(){
     alert('createItem');
   }
+  createCategory(){
+    alert('createCategory');
+  }
   render() {
     return (
       <React.Fragment>
@@ -34,8 +37,8 @@ class Header extends Component{
           <ul className="xu-nav xu-grid">
             <li className="xu-grid-1"></li>
             <li className="xu-grid-2"><img className="xu-logo" src="../imgs/logo.png"/></li>
-            <li className="xu-grid-2"><span onClick={this.openModel.bind(this,'1')}>创建条目</span></li>
-            <li className="xu-grid-2"><span onClick={this.openModel.bind(this,'2')}>创建条目2</span></li>
+            <li className="xu-grid-2"><span onClick={this.openModel.bind(this,'1')}>创建类别</span></li>
+            <li className="xu-grid-2"><span onClick={this.openModel.bind(this,'2')}>创建条目</span></li>
             <li className="xu-grid-3"><input type="text"/></li>
             <li className="xu-grid-1"></li>
             <li className="xu-grid-2"><a href="javascript:void();">管理</a></li>
--- client/main.js
+++ client/main.js
@@ -4,5 +4,6 @@ import Index from './js/index';
 import './css/reset.css';
 import './css/styles.css';
 import './css/xui.css';
+import './css/popup.css';
 
 render(<Index/> , document.getElementById('root'));
