b6402aa1fc4c3a47de7d86684d3440374264a504
owenzh <zhangxudong_sun@sina.com>
2018-10-19 17:04:31 +0800

    update div wrapper

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { Router, Route, Link, IndexRoute } from 'react-router';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Category from './components/category';
 import Home from './layout/home';
@@ -9,18 +9,20 @@ class Index extends Component{
     render(){
         return (
             <Router>
-            <div>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/category">Category</Link></li>
-                </ul>
+                <div>
+                    <div>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/category">Category</Link></li>
+                        </ul>
+                    </div>
+                    <div>{this.props.children}</div>
+                    <Footer/>
+                    <Route path="/" component={Home}>
+                    {/* <IndexRoute component={Home}/> */}
+                         <Route path="category" component={Category} />
+                    </Route>
             </div>
-            <div>{this.props.children}</div>
-            <Footer/>
-          <Route path="/" component={Home}>
-            <IndexRoute component={Home}/>
-            <Route path="category" component={Category} />
-          </Route>
         </Router>
         );
     }
