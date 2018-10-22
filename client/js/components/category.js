a8cde1e30ca4fdfe1ff7630dab484304ab665d20
owenzh <zhangxudong_sun@sina.com>
2018-10-22 16:46:25 +0800

    update hash router usage.

--- client/js/components/category.js
+++ client/js/components/category.js
@@ -1,14 +1,37 @@
 import  React,{Component} from 'react';
-// import ReactDom from 'react-dom';
 
 export default class Category extends Component {
     constructor(props){
         super(props);
+        console.log(props);
+        this.state = {content:null}
     }
+    checkUpdate() {
+        let item = this.props.path_name;
+        let contentMap = {
+            hot: 'This is list of hot product.',
+            man: 'This is list of man product.',
+            lady: 'This is list of lady product.',
+            children: 'This is list of children product.'
+        };
+        
+        this.setState({
+            content: contentMap[item]
+        }, () => {
+            console.log('set content already.');
+        });
+    }
+    componentWillMount() {
+        this.checkUpdate();
+    }
+    // shouldComponentUpdate(){
+    //     return true;
+    // }
     render(){
         return (
             <React.Fragment>
-                 Category Component
+                Category Component <br/>
+                {this.state.content}
             </React.Fragment>
         );
     }
--- client/js/index.js
+++ client/js/index.js
@@ -1,29 +1,68 @@
 import React,{Component} from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-import Category from './components/category';
 import Home from './layout/home';
-import Footer from './layout/footer';
+import Category from './components/category';
+
+const CateItem = ({text,toLink}) => (
+    <Link to={toLink} replace={true}>{text}</Link> //add replace atrr to hash history cannot push state; it is ignored
+);
+const RouteItem = ({key,path,pathName}) => (
+    <Route exact key={key} path={path} render={props=><Category {...props} path_name={pathName}/>}/>
+);
 
 class Index extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {category_map: null}
+    }
+    componentWillMount() {
+        let category = [{
+            id: 1001,
+            text: "hot",
+            name: "hot"
+        }, {
+            id: 1002,
+            text: "man",
+            name: "man"
+        }, {
+            id: 1003,
+            text: "lady",
+            name: "lady"
+        }, {
+            id: 1004,
+            text: "children",
+            name: "children"
+        }];
+        this.setState({
+            category_map: category
+        }, () => {
+            console.log('set category already.');
+        });
+    } 
     render(){
+        let cate = this.state.category_map;
+        const listItems = cate.map((cate) =>
+          <li key={cate.id} style={{padding: "10px"}}> 
+            <CateItem text={cate.text} toLink={"/category/"+cate.name}/>
+          </li>  
+        );
+        //Init the route for special link
+        const routeItems = cate.map((cate)=>
+            <RouteItem key={cate.id} path={"/category/"+cate.name} pathName={cate.name}/>
+        );
         return (
             <Router>
                 <div>
-                    <div>
-                        <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/category">Category</Link></li>
-                        </ul>
-                    </div>
-                    <div>{this.props.children}</div>
-                    <Footer/>
-                    <Route path="/" component={Home}>
-                    {/* <IndexRoute component={Home}/> */}
-                         <Route path="category" component={Category} />
-                    </Route>
-            </div>
-        </Router>
+                    <ul>
+                        {listItems}
+                    </ul>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        {routeItems}
+                    </Switch>
+                </div>
+            </Router>
         );
     }
 }
