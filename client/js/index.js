9bdee70d0a05451bdbd0eef53852dd4cf70a1ebd
owenzh <zhangxudong_sun@sina.com>
2018-10-23 11:43:57 +0800

    update router adjust

--- client/js/index.js
+++ client/js/index.js
@@ -7,9 +7,13 @@ import Category from './components/category';
 const CateItem = ({text,toLink,selectedClass}) => (
     <NavLink exact replace to={toLink} activeClassName={selectedClass}>{text}</NavLink> //add replace atrr to hash history cannot push state; it is ignored
 );
-const RouteItem = ({key,path,pathName}) => (
-    <Route exact key={key} path={path} render={props=><Category {...props} path_name={pathName}/>}/>
-);
+const RouteItem = ({key,path,pathName}) => {
+    if(pathName == "home") {
+        return (<Route exact path="/" component={Home}/>);
+    } else {    
+        return (<Route exact key={key} path={path} render={props=><Category {...props} path_name={pathName}/>}/>);
+    }        
+};
 
 class Index extends Component{
     constructor(props) {
@@ -18,21 +22,30 @@ class Index extends Component{
     }
     componentWillMount() {
         let category = [{
+            id: 1000,
+            text: "首页",
+            name: "home",
+            toLink: "/"
+        }, {
             id: 1001,
-            text: "hot",
-            name: "hot"
+            text: "热卖",
+            name: "hot",
+            toLink: "/category/hot"
         }, {
             id: 1002,
-            text: "man",
-            name: "man"
+            text: "男装",
+            name: "man",
+            toLink: "/category/man"
         }, {
             id: 1003,
-            text: "lady",
-            name: "lady"
+            text: "女装",
+            name: "lady",
+            toLink: "/category/lady"
         }, {
             id: 1004,
-            text: "children",
-            name: "children"
+            text: "儿童",
+            name: "children",
+            toLink: "/category/children"
         }];
         this.setState({
             category_map: category
@@ -44,26 +57,22 @@ class Index extends Component{
         let cate = this.state.category_map;
         const listItems = cate.map((cate) =>
           <li key={cate.id}> 
-            <CateItem text={cate.text} toLink={"/category/"+cate.name} selectedClass={"nav_selected"}/>
+            <CateItem text={cate.text} toLink={cate.toLink} selectedClass={"nav_selected"}/>
           </li>  
         );
         //Init the route for special link
         const routeItems = cate.map((cate)=>
-            <RouteItem key={cate.id} path={"/category/"+cate.name} pathName={cate.name}/>
+            <RouteItem key={cate.id} path={cate.toLink} pathName={cate.name}/>
         );
         return (
             <Router>
                 <div className="index xu-grid">
                     <ul className="sidebar xu-grid-1">
-                        <li>
-                            <NavLink exact replace to='/' activeClassName="nav_selected">Home</NavLink>
-                        </li>
                         {listItems}
                     </ul>
                     
                     <div className="content xu-grid-10">
                         <Switch>
-                            <Route exact path="/" component={Home}/>
                             {routeItems}
                         </Switch>
                     </div>
