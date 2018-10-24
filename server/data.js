ea6159d7915b37a560b9949e7152961bd9ef0ab7
owenzh <zhangxudong_sun@sina.com>
2018-10-24 17:01:06 +0800

    fake data

--- server/data.js
+++ server/data.js
@@ -1,30 +1,141 @@
 const prod = {
-    hot: [
-        {
-            id: 10001,
-            prod_name: 'JEEP 吉普 男式 纯色休闲短袖衬衫 JS10WH102',
-            prod_short: 'JEEP 吉普',
-            prod_price: 149,
-            price_unit: 'RMB',
-            prod_type: 'man'
-        }
-        ,{
-            id: 10003,
-            prod_name: 'New Balance 女 休闲跑步鞋 574系列 WL574ESP-B',
-            prod_short: 'New Balance 休闲跑步鞋',
-            prod_price: 59,
-            price_unit: 'RMB',
-            prod_type: 'lady'
-        }
-        ,{
-            id: 10011,
-            prod_name: 'LEGO 乐高 拼插类 玩具 DUPLO 得宝系列 贝儿公主的下午茶 10877 2-5岁 婴幼',
-            prod_short: 'LEGO 乐高',
-            prod_price: 209.20,
-            price_unit: 'RMB',
-            prod_type: 'children'
-        }
-    ]
+    hot: [{
+        id: 11569,
+        prod_name: 'JEEP 吉普 男式 纯色休闲短袖衬衫 JS10WH102',
+        prod_short: 'JEEP 吉普',
+        prod_price: 149,
+        price_unit: 'RMB',
+        prod_type: 'man'
+    }, {
+        id: 11626,
+        prod_name: 'New Balance 女 休闲跑步鞋 574系列 WL574ESP-B',
+        prod_short: 'New Balance 休闲跑步鞋',
+        prod_price: 59,
+        price_unit: 'RMB',
+        prod_type: 'lady'
+    }, {
+        id: 59656,
+        prod_name: 'LEGO 乐高 拼插类 玩具 DUPLO 得宝系列 贝儿公主的下午茶 10877 2-5岁 婴幼',
+        prod_short: 'LEGO 乐高',
+        prod_price: 209.20,
+        price_unit: 'RMB',
+        prod_type: 'children'
+    }],
+    man: [{
+        id: 88673,
+        prod_name: 'MPSMOVE思慕夫 2018秋冬新款圆领男士长袖卫衣t恤男装秋装打底衫秋冬上衣百搭衣服KW648',
+        prod_short: 'MPSMOVE思慕夫',
+        prod_price: 78,
+        price_unit: 'RMB',
+        prod_type: 'man'
+    }, {
+        id: 69186,
+        prod_name: 'Futex Plus 男式 短袖衬衫 1013101',
+        prod_short: 'Futex Plus 短袖衬衫',
+        prod_price: 69,
+        price_unit: 'RMB',
+        prod_type: 'man'
+    }, {
+        id: 38695,
+        prod_name: '几何森林热带夏威夷蜡染衬衫 BY johari WEST ',
+        prod_short: '几何森林 衬衫',
+        prod_price: 661.85,
+        price_unit: 'RMB',
+        prod_type: 'man'
+    }, {
+        id: 77444,
+        prod_name: '牧马人 authentics 男式长袖帆布衬衫 ',
+        prod_short: '牧马人 authentics',
+        prod_price: 136.18,
+        price_unit: 'RMB',
+        prod_type: 'man'
+    }, {
+        id: 45765,
+        prod_name: 'Lee 男士牛仔夹克',
+        prod_short: 'Lee 男士牛仔夹克',
+        prod_price: 236.03,
+        price_unit: 'RMB',
+        prod_type: 'man'
+    }, {
+        id: 68263,
+        prod_name: 'Lee 男式现代系列极限运动 运动牛仔裤',
+        prod_short: 'Lee 运动牛仔裤',
+        prod_price: 661.85,
+        price_unit: 'RMB',
+        prod_type: 'man'
+    }, {
+        id: 38695,
+        prod_name: 'Goralon 新款男士外套秋冬装休闲连帽夹克外套韩版青年男装上衣百搭贴布夹克衫男外套',
+        prod_short: 'Goralon 新款男士外套',
+        prod_price: 168.00,
+        price_unit: 'RMB',
+        prod_type: 'man'
+    }, {
+        id: 97009,
+        prod_name: 'Columbia 男式 utilizer 夹克 ',
+        prod_short: 'Columbia utilizer 夹克 ',
+        prod_price: 344.43,
+        price_unit: 'RMB',
+        prod_type: 'man'
+    }],
+    lady: [{
+        id: 32880,
+        prod_name: '杨埔 秋款女2018新款一字领雪纺衫荷叶边网纱上衣长袖宽松仙洋气小衫休闲女士衬衫',
+        prod_short: '杨埔 秋款女2018',
+        prod_price: 92.00,
+        price_unit: 'RMB',
+        prod_type: 'lady'
+    }, {
+        id: 72722,
+        prod_name: 'Michael Kors 迈克高仕女士装饰纽扣衬衫',
+        prod_short: 'Michael Kors 迈克高仕女士装饰纽扣衬衫',
+        prod_price: 217.45,
+        price_unit: 'RMB',
+        prod_type: 'lady'
+    }, {
+        id: 69252,
+        prod_name: 'threads 4 thought 女式 estera 格子纽扣衬衫 ',
+        prod_short: 'threads 4 格子纽扣衬衫',
+        prod_price: 190.94,
+        price_unit: 'RMB',
+        prod_type: 'lady'
+    }, {
+        id: 45968,
+        prod_name: '盔甲 LUX 04277，女式条纹长袖 t 恤 ',
+        prod_short: '盔甲 LUX 04277',
+        prod_price: 309.20,
+        price_unit: 'RMB',
+        prod_type: 'lady'
+    }],
+    children: [{
+        id: 93318,
+        prod_name: 'adidas Kids 阿迪达斯 男童 小童针织套服 短袖 LB SS TEE 34 SE',
+        prod_short: 'adidas Kids 小童针织套服',
+        prod_price: 159.00,
+        price_unit: 'RMB',
+        prod_type: 'children'
+    }, {
+        id: 67222,
+        prod_name: 'adidas Kids 阿迪达斯 男童 大童针织夹克 YB ZNE HD 2.0',
+        prod_short: 'adidas Kids 大童针织夹克',
+        prod_price: 259.00,
+        price_unit: 'RMB',
+        prod_type: 'children'
+    }, {
+        id: 91288,
+        prod_name: 'adidas Kids 阿迪达斯 女童 小童套头卫衣 LG CREWNECK',
+        prod_short: 'adidas Kids 小童套头卫衣',
+        prod_price: 109.00,
+        price_unit: 'RMB',
+        prod_type: 'children'
+    }, {
+        id: 37236,
+        prod_name: 'Disney 迪士尼童装 女童 套头卫衣 KVM8S1KTKG2305XA',
+        prod_short: 'Disney 迪士尼童装',
+        prod_price: 69.00,
+        price_unit: 'RMB',
+        prod_type: 'children'
+    }]
 }
 
 module.exports = prod;
